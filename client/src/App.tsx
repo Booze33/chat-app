1fd2a66c9801454704ead74ac3d573bec604ba35
Booze33 <tebetisloh@gmail.com>
2024-04-15 02:28:03 +0200

    Added auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Signup from './components/auth/signUp';
+import Signin from './components/auth/signIn';
+import Home from './components/pages/Home';
+import { setUser } from './redux/auth/authSlice';
 
-const App = () => {
-  const key = import.meta.env;
-  console.log(key);
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const storedUID = localStorage.getItem('sessionUID');
+    if (storedUID) {
+      const storedUser = JSON.parse(localStorage.getItem('user'));
+      dispatch(setUser({ uid: storedUID, ...storedUser }));
+    }
+  }, [dispatch]);
 
   return (
-    <div className="bg-red-500 text-[#aa4242]">
-      App4
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/sign_in" element={<Signin />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
